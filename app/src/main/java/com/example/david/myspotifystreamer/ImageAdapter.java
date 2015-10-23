b16bedcf45740a273838b28ec2fab2cdd0abaca8
yuweipei <yuweipei@seas.upenn.edu>
2015-10-23 01:58:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/david/myspotifystreamer/ImageAdapter.java
@@ -0,0 +1,84 @@
+package com.example.david.myspotifystreamer;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by David on 10/20/15.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    private String[] urlArray = {
+            "http://image.tmdb.org/t/p/w185/vlTPQANjLYTebzFJM1G4KeON0cb.jpg",
+            "http://image.tmdb.org/t/p/w185/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return urlArray.length;
+    }
+
+    @Override
+    public String getItem(int position) {
+        return urlArray[position];
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            //imageView.setLayoutParams(new GridView.LayoutParams(120,120));
+            //imageView.setLayoutParams(mImageViewLayoutParams);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            //imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        //imageView.setImageResource(mThumbIds[position]);
+
+        Picasso.with(imageView.getContext())
+                .load(getItem(position))
+                .noFade()//.resize(250,250)
+                .into(imageView);
+        /*Picasso.with(mContext)
+                .load("http://imgur.com/M3ny4P3")
+                .noFade().resize(250,250)
+                .into(imageView);
+        */
+        //Picasso.with(mContext).load("http://imgur.com/MEbfVbN").into(imageView);
+        //Picasso.with(mContext).load("http://imgur.com/uVCNc1C").into(imageView);
+
+        return imageView;
+    }
+
+    public void clear() {
+        urlArray = new String[0];
+    }
+
+    public void addAll(String[] input) {
+        urlArray = new String[input.length];
+        for (int i = 0; i < input.length; i++) {
+            urlArray[i] = input[i];
+        }
+        notifyDataSetChanged();
+    }
+
+}
---
+++ app/src/main/java/com/example/david/myspotifystreamer/MainActivityFragment.java
@@ -0,0 +1,229 @@
+package com.example.david.myspotifystreamer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ImageAdapter imageAdapter;
+
+    private GridView gridView;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Use its own movieview to inflate the view
+        // use xml view to create a menu option.
+        inflater.inflate(R.menu.movieview,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle refresh here
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchMovieTask fetchMovieTask = new FetchMovieTask();
+            fetchMovieTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //setContentView(R.layout.activity_main);
+
+        // Use ImageAdapter to create GridView
+
+        imageAdapter = new ImageAdapter(getActivity());
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        gridView = (GridView) rootView.findViewById(R.id.movieGrid);
+        gridView.setAdapter(imageAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                /*
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+                */
+                Intent intent = new Intent(getActivity(), DetailActivityFragment.class);
+            }
+        });
+
+        return rootView;
+    }
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, String[]> {
+
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+        private String[] getMovieDataFromJson(String JsonStr)
+                throws JSONException {
+
+            // These are names of the json object that need to be extracted
+            final String OWM_RESULTS = "results";
+            final String OWM_POSTER = "poster_path";
+            final String OWM_TITLE = "original_title";
+            final String OWM_OVERVIEW = "overview";
+            final String OWM_VOTE = "vote_average";
+
+            JSONObject movieJson = new JSONObject(JsonStr);
+
+            JSONArray resultArray = movieJson.getJSONArray(OWM_RESULTS);
+
+            String[] resultStrs = new String[resultArray.length()];
+            for (int i = 0; i < resultArray.length(); i++) {
+
+                // Get the JSON Object of each movie
+                JSONObject eachMovie = resultArray.getJSONObject(i);
+
+                //
+                String postalURL = eachMovie.getString(OWM_POSTER);
+                String actualURL = "http://image.tmdb.org/t/p/w185" + postalURL;
+                Log.v(LOG_TAG, "Postal_url:" + actualURL);
+                resultStrs[i] = actualURL;
+            }
+
+            return resultStrs;
+         }
+
+
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+
+            if (params == null) {
+                return null;
+            }
+
+            // Declare out side try/catch
+            // so that they can be closed in the finally block
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string
+            String jsonStr = null;
+
+            //
+            String searchArea = "movie";
+            String[] sortParamList = {
+                    "popularity.desc",
+                    "vote_average.desc",
+                    "vote_count.desc"
+            };
+            String myID = "d3fa940065011fb83cc29169d05e8019";
+            try {
+                // Construct the URL for the
+                final String REQUEST_BASE_URL = "http://api.themoviedb.org/3/discover";
+                final String QUERY_PARAM = "";
+                final String APP_KEY = "api_key";
+                final String SORT_PARAM = "sort_by";
+
+                Uri builtUri = Uri.parse(REQUEST_BASE_URL).buildUpon()
+                        .appendPath(searchArea)
+                        .appendQueryParameter(SORT_PARAM, sortParamList[0])
+                        .appendQueryParameter(APP_KEY, myID)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI: " + builtUri.toString());
+
+                // Create the request to themoviedb
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer stringBuffer = new StringBuffer();
+                if ( inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuffer.append(line + "\n");
+                }
+
+                if (stringBuffer.length() == 0) {
+                    // Stream was empty
+                    return null;
+                }
+
+                jsonStr = stringBuffer.toString();
+                //Log.v(LOG_TAG, "JSON String: " + jsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+            } finally {
+                if (urlConnection != null) {
+                    // still connected
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, e.getMessage(), e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(jsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                imageAdapter.clear();
+                imageAdapter.addAll(strings);
+            }
+        }
+    }
+}
